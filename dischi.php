12449e1f9cf4b1fc66d13219f2ffa26e8aa54f9e
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-09-11 16:17:20 +0200

    struttura base +oggetto json e collego a file php

---
+++ dischi.php
@@ -0,0 +1,10 @@
+<?php
+
+// leggo il contenuto del file 
+$string = file_get_contents("dischi.json");
+
+// converto il json da stringa ad array assocciativo 
+$card = json_decode($string, true);
+var_dump($string);
+var_dump($card);
+?>
---
+++ js/script.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+data(){
+    return{
+        dischi:[]
+    }
+},
+mounted(){
+axios.get()
+.then((response) => {
+
+});
+}
+
+
+});
+console.log(app);
+app.mount("#app")
